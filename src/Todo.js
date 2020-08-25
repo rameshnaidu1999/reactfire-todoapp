cb8cd1e5ce2afe6cec2726f94b01b795a616741b
rameshnaidu1999 <kasularameshnaidu@gmail.com>
2020-08-25 12:50:50 +0530

    completed app

--- src/Todo.js
+++ src/Todo.js
@@ -1,44 +1,75 @@
 import React, { useState } from 'react'
-import { List, ListItem, ListItemText, ListItemAvatar, Button, Modal } from '@material-ui/core'
+import { List, ListItem, ListItemText, ListItemAvatar, Button, 
+    Modal, FormControl, InputLabel, Input, Backdrop, Fade } from '@material-ui/core'
 import './Todo.css'
 import db from './firebase'
 import {DeleteForever} from '@material-ui/icons' 
 import {makeStyles} from '@material-ui/core/styles'
- 
-const useStyles = makeStyles((theme)=> ({
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadow[5],
-        padding: theme.spaacing(2,4,3),
 
+
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
-}))
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 
 function Todo(props) {
     const classes = useStyles();
     const [open, setOpen] = useState(false)
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    return (
-        <div>
-        <Modal
-            open={open}
-            onClose={e => setOpen(false)}>
-            <div>
-                <h1>I am modal</h1>
-                <button onClick={e => setOpen(false)}></button>
+    const [input, setInput] = useState()
+
+
+    const updateTodo = (event) => {
+        event.preventDefault();
+        db.collection('todos').doc(props.todo.id).set({
+            todo: input
+        }, {merge: true})
+        setOpen(false)
+    }
+    return ( 
+        <div >
+            <div >
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                      timeout: 500,
+                    }}
+                    onClose={e => setOpen(false)}>
+                        <Fade in={open}>
+                            <div className={classes.paper}>
+                                <form>
+                                <h1>Update your Todo</h1>
+                                <FormControl>
+                                    <InputLabel>Write a Todo</InputLabel>
+                                    <Input value={input} placeholder={props.todo.todo}
+                                    onChange={event => setInput(event.target.value)} />
+                                </FormControl>
+                                <Button disabled={!input} type="submit" onClick={updateTodo} variant="contained" color="primary">Update</Button>
+                            </form>
+                            </div>
+                        </Fade>
+                </Modal>
             </div>
-        </Modal>
            <List className="todo_list">
                <ListItem>
                     <ListItemAvatar>
 
                     </ListItemAvatar>
-                   <ListItemText primary={props.todo.todo} secondary="Dummy dead line" />
+                   <ListItemText primary={props.todo.todo}  />
                </ListItem>
                <Button onClick={e => setOpen(true)} >
                Edit
